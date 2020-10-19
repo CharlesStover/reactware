4fe0bf8fbcd3d48d1c2c9ec82683813e957f474c
Charles Stover <chastove@amazon.com>
2020-10-18 23:48:27 -0700

    lift graphics

--- src/games/lift/lift.hook.ts
+++ src/games/lift/lift.hook.ts
@@ -1,15 +1,42 @@
-import { Dispatch, useCallback, useMemo } from 'react';
+import { CSSProperties, Dispatch, useCallback, useMemo } from 'react';
 import useKeyDown from '../../hooks/use-key-down';
+import spritesheet from './images/spritesheet.gif';
 import Action from './lift.action';
 import Step from './types/step';
 import mapStepToTargetKeys from './utils/map-step-to-target-keys';
 
 interface Props {
+  cycle: number;
   dispatch: Dispatch<Action>;
   step: Step;
 }
 
-export default function useLift({ dispatch, step }: Props): void {
+const SPRITE_SHEET_HEIGHT = 49;
+const SPRITE_SHEET_WIDTH = 145;
+
+function mapCycleToScale(cycle: number): number {
+  return 1 + Math.log(cycle + 1);
+}
+
+function mapStepToBackgroundPosition(step: Step): number[] {
+  switch (step) {
+    case 0:
+    case 1:
+    case 3:
+    case 4:
+      return [-1, -1];
+    case 2:
+      return [-49, -1];
+    case 5:
+      return [-97, -1];
+  }
+}
+
+export default function useLift({
+  cycle,
+  dispatch,
+  step,
+}: Props): CSSProperties {
   const targetKeys: string[] = useMemo(
     (): string[] => mapStepToTargetKeys(step),
     [step],
@@ -25,4 +52,20 @@ export default function useLift({ dispatch, step }: Props): void {
   );
 
   useKeyDown(handleKeyDown);
+
+  return useMemo((): CSSProperties => {
+    const [x, y] = mapStepToBackgroundPosition(step);
+    const scale: number = mapCycleToScale(cycle);
+    const scaledPositionX: number = Math.round(x * scale);
+    const scaledPositionY: number = Math.round(y * scale);
+    const scaledSizeHeight: number = Math.round(SPRITE_SHEET_HEIGHT * scale);
+    const scaledSizeWidth: number = Math.round(SPRITE_SHEET_WIDTH * scale);
+    return {
+      backgroundImage: `url("${spritesheet}")`,
+      backgroundPosition: `${scaledPositionX}px ${scaledPositionY}px`,
+      backgroundSize: `${scaledSizeWidth}px ${scaledSizeHeight}px`,
+      height: Math.floor(47 * scale),
+      width: Math.floor(47 * scale),
+    };
+  }, [cycle, step]);
 }
--- src/games/lift/lift.view.tsx
+++ src/games/lift/lift.view.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { CSSProperties, ReactElement } from 'react';
 import GameViewProps from '../../types/game-view-props';
 import Action from './lift.action';
 import useLift from './lift.hook';
@@ -8,11 +8,7 @@ export default function Lift({
   dispatch,
   state: { cycle, step },
 }: GameViewProps<State, Action>): ReactElement {
-  useLift({ dispatch, step });
+  const style: CSSProperties = useLift({ cycle, dispatch, step });
 
-  return (
-    <div>
-      Lift {cycle} {step}
-    </div>
-  );
+  return <div style={style} />;
 }
